99ba018cc3749cda0cea824bfb99fbc121861b22
katiegrace <katiecgrace@gmail.com>
2018-09-11 08:13:43 -0700

    lab 2A

---
+++ Unit2ALab/Unit2ALab.py
@@ -0,0 +1,20 @@
+print(3.45 + 4)
+
+print(int(3.45) + 4)
+
+print("Enter a number: ")
+ask = input()
+print(int(ask) + 5)
+
+i = 100
+j = 200
+x = 300
+y = 100
+
+print(i == j or i > x)
+
+print(i < j and i >= y)
+
+print(i != j and i != x)
+
+print(i < j and i < x)
