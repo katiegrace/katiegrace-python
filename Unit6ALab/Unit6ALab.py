dd13a2e3027248c6b78aa3b02e9983aba8dce695
Thomas Pentimonti <20PentimontiThomas@bprep.org>
2018-11-28 09:46:58 -0800

    fun

--- Unit6ALab/Unit6ALab.py
+++ Unit6ALab/Unit6ALab.py
@@ -1,45 +1,91 @@
-textCodes = {'ttyl' : 'talk to you later', 'wbu' : 'what about you', 'lol' : 'laugh out loud'}
 
-key = input("Shortcut: ")
+def main() :
 
-while (key in textCodes) :
-    print(textCodes[key])
-    add = input("Do you want to add a word? (yes or no) ")
-    if add == "yes" :
-        answer2 = input("What does this mean? ")
-        textCodes['key'] = answer2
-    elif add == "no" :
-        delete2 = input("Do you want to delete any words? (yes or no) ") == "yes"
-        if delete2 == "yes" :
-            print(textCodes)
-            takeOut = input("Which word? ")
-            del textCodes[takeOut]
-        elif delete2 != "yes" :
-            print(textCodes)
-            mod = input("Do you want to modify a word? (yes or no) ")
-            if mod == "yes" :
-                which = input("Which word? ")
-                change = input("What do you want to change it to? ")
-                textCodes[which] = change
-            else :
-                print("Ok bye")
-                quit()
+    textCodes = {'ttyl' : 'talk to you later', 'wbu' : 'what about you', 'lol' : 'laugh out loud'}
 
-ask = input("Do you want to add this word? (yes or no)")    # start here
-if ask == "yes" :
-    answer = input("What does this mean? ")
-    textCodes[key] = answer
-    print(textCodes)
-elif ask == "no" :
-    delete = input("Do you want to delete any words? (yes or no) ") == "yes"
+    key = input("Shortcut: ")
+
+    while (key in textCodes) :
+        print(textCodes[key])
+        deleteWord(textCodes)
+        modifyWord(textCodes)
+    addWord(textCodes, key)
+    deleteWord(textCodes)
+    modifyWord(textCodes)
+
+def addWord(list, key) :
+    ask = input("Do you want to add this word? (yes or no)")    # start here
+    if ask == "yes" :
+        answer = input("What does this mean? ")
+        list[key] = answer
+        print(list)
+    elif ask == "no" :
+        deleteWord(list)
+
+
+def deleteWord(list) :
+    delete = input("Do you want to delete any words? (yes or no) ")
     if delete == "yes" :
-        print(textCodes)
+        print(list)
         takeOut = input("Which word? ")
-        del textCodes[takeOut]
+        del list[takeOut]
+        print(list)
     elif delete != "yes" :
-        print(textCodes)
-        mod = input("Do you want to modify a word? (yes or no) ")
-        if mod == "yes" :
-            which = input("Which word? ")
-            change = input("What do you want to change it to? ")
-            textCodes[which] = change
+        print(list)
+        modifyWord(list)
+
+def modifyWord(list) :
+    mod = input("Do you want to modify a word? (yes or no) ")
+    if mod == "yes" :
+        which = input("Which word? ")
+        change = input("What do you want to change it to? ")
+        list[which] = change
+        print(list)
+    if mod != "yes" :
+        print("Ok bye")
+        quit()
+
+
+main()
+
+
+
+    # add = input("Do you want to add a word? (yes or no) ")
+    # if add == "yes" :
+    #     answer2 = input("What does this mean? ")
+    #     textCodes['key'] = answer2
+    # elif add == "no" :
+    #     delete2 = input("Do you want to delete any words? (yes or no) ") == "yes"
+    #     if delete2 == "yes" :
+    #         print(textCodes)
+    #         takeOut = input("Which of these words? ")
+    #         del textCodes[takeOut]
+    #     elif delete2 != "yes" :
+    #         print(textCodes)
+    #         mod = input("Do you want to modify a word? (yes or no) ")
+    #         if mod == "yes" :
+    #             which = input("Which word? ")
+    #             change = input("What do you want to change it to? ")
+    #             textCodes[which] = change
+    #         else :
+    #             print("Ok bye")
+    #             quit()
+
+# ask = input("Do you want to add this word? (yes or no)")    # start here
+# if ask == "yes" :
+#     answer = input("What does this mean? ")
+#     textCodes[key] = answer
+#     print(textCodes)
+# elif ask == "no" :
+#     delete = input("Do you want to delete any words? (yes or no) ") == "yes"
+#     if delete == "yes" :
+#         print(textCodes)
+#         takeOut = input("Which word? ")
+#         del textCodes[takeOut]
+#     elif delete != "yes" :
+#         print(textCodes)
+#         mod = input("Do you want to modify a word? (yes or no) ")
+#         if mod == "yes" :
+#             which = input("Which word? ")
+#             change = input("What do you want to change it to? ")
+#             textCodes[which] = change
